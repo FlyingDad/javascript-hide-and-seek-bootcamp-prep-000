fcae31b34c9c5940e92d0d62476e84c63dc55b8e
Michael Kroth <mkroth65@gmail.com>
2018-02-27 01:29:00 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  const nested = document.querySelector('#nested');
+  const target = nested.querySelector('.target');
+  return target;
+}
+
+function increaseRankBy(n){
+	const rankedLi = document.querySelectorAll('.ranked-list li');
+	//console.log(rankedLi);
+	for (let i = 0; i < rankedLi.length; i++) {
+		var num = parseInt(rankedLi[i].innerHTML);
+		rankedLi[i].innerHTML =  num + n;//(i + 1).toString();
+	}
+}
